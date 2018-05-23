197b2323a126e443f400093d4e93e9a723e6542c
Rajeev Belani <rajeev@woodsofttechnologies.com>
2018-05-23 15:03:57 +0530

    Website title

--- static.config.js
+++ static.config.js
@@ -197,6 +197,7 @@ export default {
       return (
         <Html>
           <Head>
+            <title>Magnus Medi</title>
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
