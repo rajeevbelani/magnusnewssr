bef18cd50e4e6e2789e579a7a3c807d69f693007
Rajeev Belani <rajeev@woodsofttechnologies.com>
2018-05-25 18:48:24 +0530

    Adding Images home

--- static.config.js
+++ static.config.js
@@ -1,12 +1,12 @@
-import axios from 'axios'
-import React, { Component } from 'react'
-import { ServerStyleSheet } from 'styled-components'
 /*
 * For Less Support
 * */
-import autoprefixer from 'autoprefixer'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
-import postcssFlexbugsFixes from 'postcss-flexbugs-fixes'
+import autoprefixer from 'autoprefixer';
+import axios from 'axios';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import postcssFlexbugsFixes from 'postcss-flexbugs-fixes';
+import React, { Component } from 'react';
+import { ServerStyleSheet } from 'styled-components';
 
 /*
 * For TypeScript Support
@@ -206,7 +206,6 @@ export default {
             <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-            <link rel="manifest" href="/site.webmanifest" />
             <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
             <meta name="msapplication-TileColor" content="#da532c" />
             <meta name="theme-color" content="#ffffff" />
